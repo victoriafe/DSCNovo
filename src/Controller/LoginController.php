9b6fd1363502dcedf0a40876d0b076aa44e47a71
Victória Felipe <victoriadamascena222@gmail.com>
2024-10-19 13:11:42 -0300

    Usa template base no login

--- src/Controller/LoginController.php
+++ src/Controller/LoginController.php
@@ -21,6 +21,7 @@ class LoginController extends AbstractController
         return $this->render('login/login.html.twig', [
             'last_username' => $lastUsername,
             'error' => $error,
+            'hideNav' => true,
         ]);
     }
 
